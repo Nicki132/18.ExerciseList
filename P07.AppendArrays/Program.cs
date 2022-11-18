8fbe10cb76fd86688f539dff57c78fdafc240df2
Nikolay Stoyanov <nikitig1987@abv.bg>
2022-11-18 15:56:18 +0100

    Add project files.

---
+++ P07.AppendArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                numbers.AddRange(input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
